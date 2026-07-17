f6f942c2e233252cd3cdacfafa8938006a33b824
leejaebaek5342 <leejaebaek5342@gmail.com>
2026-07-17 19:45:29 +0900

    style: format UART RX headers

--- stm32/conveyor-controller/Application/Inc/uart_rx.h
+++ stm32/conveyor-controller/Application/Inc/uart_rx.h
@@ -3,16 +3,14 @@
 
 #include <stdint.h>
 
-#include "stm32f4xx_hal.h"
 #include "app_messages.h"
+#include "stm32f4xx_hal.h"
 
 HAL_StatusTypeDef uart_rx_start(void);
 
-HAL_StatusTypeDef uart_rx_restart(
-    app_uart_channel_t channel);
+HAL_StatusTypeDef uart_rx_restart(app_uart_channel_t channel);
 
-uint32_t uart_rx_take_error(
-    app_uart_channel_t channel);
+uint32_t uart_rx_take_error(app_uart_channel_t channel);
 
 uint32_t uart_rx_get_dropped_chunk_count(void);
 
